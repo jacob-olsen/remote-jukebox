c64e4aa6faea18e2fed9f0467ccb4c8240f7a754
jacob olsen <jacobspil@hotmail.dk>
2025-02-01 19:08:09 +0100

    i think that is all parts for this class

--- service/player_manger.py
+++ service/player_manger.py
@@ -2,14 +2,23 @@ import vlc
 
 class Player:
     def __init__(self):
+        self.__updateEventList = []
+        self.__doneEventList = []
         self.__instance = vlc.Instance()
         self.__player = self.__instance.media_player_new()
+        self.__songId = 0
         self.__media = None
+
+        self.__event_manager = self.__player.event_manager()
+        self.__event_manager.event_attach(vlc.EventType.MediaPlayerEndReached, self.__done_event_triger)
+
+        
 
     def __str__(self):
         return f"{self.__player.get_time()}-{self.__player.get_length()}:ms {int(self.__player.get_position()*100)}%"
 
-    def setSong(self, filepath):
+    def setSong(self, filepath, songId):
+        self.__songId = songId
         self.__player.stop()
         self.__media = self.__instance.media_new(filepath)
         self.__player.set_media(self.__media)
@@ -17,28 +26,50 @@ class Player:
 
     def play(self):
         self.__player.play()
+        self.__status_event_triger()
     
     def pause(self):
         self.__player.pause()
+        self.__status_event_triger()
 
+    def skip(self, offset):
+        pos = self.__player.get_time() + offset
+        self.__player.stop()
+        if pos < 0:
+            pos = 0
+        elif pos >= self.__player.get_length():
+            self.__player.stop()
+            self.__done_event_triger()
+            return
+        self.__player.set_time(pos)
+        self.__player.play()
+        self.__status_event_triger()
+
+    def set(self, pos):
+        if pos < 0:
+            pos = 0
+        elif pos >= self.__player.get_length():
+            self.__player.stop()
+            self.__done_event_triger()
+        self.__player.set_time(pos)
+
+    def status(self):
+        return {"ID":self.__songId,
+                "playing": self.__player.is_playing(),
+                "play_time":self.__player.get_time(),
+                "length":self.__player.get_length(),
+                "position":int(self.__player.get_position()*100)}
     
+    def status_event_add(self, listener):
+        self.__updateEventList.append(listener)
 
+    def __status_event_triger(self):
+        for func in self.__updateEventList:
+            func()
+    
+    def done_event_add(self, listener):
+        self.__doneEventList.append(listener)
 
-
-
-#testing
-new = Player()
-new.setSong("/home/jacob/Music/Blacksmith, Blacksmith.opus")
-new.play()
-
-print(str(new))
-input()
-new.pause()
-print(str(new))
-input()
-new.play()
-
-while True:
-    print(str(new))
-    input()
-
+    def __done_event_triger(self):
+        for func in self.__doneEventList:
+            func()
