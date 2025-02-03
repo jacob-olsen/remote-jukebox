ef530f870af0a8fbaa97c06e2b1b9a34fe341eb3
jacob olsen <jacobspil@hotmail.dk>
2025-02-03 15:57:32 +0100

    addet some api calls

--- app.py
+++ app.py
@@ -1,2 +1,34 @@
 from flask import Flask, render_template, request, url_for, redirect
-from service.service import Service
+
+import service.master
+
+app = Flask(__name__)
+manger = service.master.Manger()
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
+    
+@app.route("/api/play")
+def play():
+    manger.play()
+    return "ok"
+
+@app.route("/api/pause")
+def pause():
+    manger.pause()
+    return "ok"
+
+@app.route("/api/skipforward")
+def skipForward():
+    manger.skip(30000)
+    return "ok"
+
+@app.route("/api/skipbackward")
+def skipBackward():
+    manger.skip(-30000)
+    return "ok"
+
+
+if __name__=='__main__':
+    app.run(debug=True)
