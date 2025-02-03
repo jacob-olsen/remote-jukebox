ed00cab1ca27939acb0dcdce9a6b0a133168fd8d
jacob olsen <jacobspil@hotmail.dk>
2025-02-03 11:44:43 +0100

    fixet playlist

--- service/master.py
+++ service/master.py
@@ -2,6 +2,8 @@ import player_manger
 import stroge_manger
 import time
 
+import threading
+
 class Manger:
     def __init__(self):
         self.__songs = stroge_manger.Songs()
@@ -11,26 +13,37 @@ class Manger:
         self.__playListPos = 0
         self.__loop = 0 # 0=playList 1=loopList 2=loopSong
 
-        #the event systme vikker til at loker op når den bruger event fra vlc til at skriver til vlc (NEED FIX)
-        #self.__player.done_event_add(self.__songDone)
+        self.__condition = threading.Condition()
+        threading.Thread(target=self.__songDone, daemon=True).start()
+
+        self.__player.done_event_add(self.__songDoneEvent)
+
+    def __songDoneEvent(self):
+        with self.__condition:
+            self.__condition.notify_all()
 
     def __songDone(self):
-        print("song done")
-        if self.__loop == 2:
-            print("replaing")
-            self.__player.set(0)
-            self.__player.play()
-        else:
-            if self.__playListPos < len(self.__playList):
-                self.__playListPos += 1
-                print(f"start playlist-index:{self.__playListPos} song-id:{self.__playList[self.__playListPos]}")
-                self.setSong(self.__playList[self.__playListPos])
-            elif self.__loop == 1:
-                print("reset list pos")
-                self.__playListPos = 0
-                print(f"start playlist-index:{self.__playListPos} song-id:{self.__playList[self.__playListPos]}")
-                self.setSong(self.__playList[self.__playListPos])
+        while True:
+            with self.__condition:
+                self.__condition.wait()
+                print("song done")
+                if self.__loop == 2:
+                    print("replaing")
+                    self.__player.set(0)
+                    self.__player.play()
+                else:
+                    if self.__playListPos < len(self.__playList):
+                        self.__playListPos += 1
+                        print(f"start playlist-index:{self.__playListPos} song-id:{self.__playList[self.__playListPos]}")
+                        self.setSong(self.__playList[self.__playListPos])
+                    elif self.__loop == 1:
+                        print("reset list pos")
+                        self.__playListPos = 0
+                        print(f"start playlist-index:{self.__playListPos} song-id:{self.__playList[self.__playListPos]}")
+                        self.setSong(self.__playList[self.__playListPos])
 
+
+        
     def setSong(self, ID):
         self.__player.setSong(self.__songs.findSongPath(ID),ID)
 
